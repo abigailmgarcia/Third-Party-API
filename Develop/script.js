7ac182c00cf459daf9edb9b327e5e6670ce56833
abigailmgarcia <abigailmg31@gmail.com>
2023-06-12 23:50:52 -0500

    console.log button clicked

--- Develop/script.js
+++ Develop/script.js
@@ -3,9 +3,10 @@
 // in the html.
 $(function () {
   // TODO: Add a listener for click events on the save button.
-  $('.saveBtn').on('click', function() {});
-  //This code should
-  // use the id in the containing time-block as a key to save the user input in
+  $('.saveBtn').on('click', function() {
+  console.log("button clicked");
+});
+  // This code should use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
