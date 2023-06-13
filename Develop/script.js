0b690670f48a0c2e25bd568f3c9c222a18f987c7
abigailmgarcia <abigailmg31@gmail.com>
2023-06-13 00:06:16 -0500

    created variables for time block and user input based off the id in the containing time block

--- Develop/script.js
+++ Develop/script.js
@@ -5,10 +5,15 @@ $(function () {
   // TODO: Add a listener for click events on the save button.
   $('.saveBtn').on('click', function() {
   console.log("button clicked");
-});
+
   // This code should use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
+  // function?
+  const timeBlock = $(this).parent().attribute('id');
+  const userInput = $(this).sibilings('.description');
+});
+
+  //How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
@@ -17,7 +22,10 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
+
+  $('.time-block').each(function(){
+
+  })
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
